e193f14e6631dc3948d2ddc355fd304c2587bb7b
Volodymyr Chernetskyi <v.v.chernetskyi@gmail.com>
2019-10-09 16:57:46 +0300

    Hermann made everything work

--- mycat.c
+++ mycat.c
@@ -1,8 +1,10 @@
+#include <ctype.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <getopt.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include <unistd.h>
 
 #include "mycat.h"
@@ -26,6 +28,10 @@ int main(int argc, char *argv[]) {
 
     size_t passed_files = argc - 1 - invisible_to_hex;
     int *opened_files = (int *) malloc(sizeof(int) * passed_files);
+    if (opened_files == NULL) {
+        error(malloc_error);
+        _exit(EXIT_FAILURE);
+    }
     size_t j = 0;
     for (size_t i = 1; i < argc; ++i) {
         if (strncmp(argv[i], "-A", strlen(argv[i]))) {
@@ -35,35 +41,52 @@ int main(int argc, char *argv[]) {
                 for (size_t k = 0; k < j; k++)
                     close(opened_files[k]);
                 free(opened_files);
-                _exit(1);
+                _exit(EXIT_FAILURE);
             }
             opened_files[j++] = fd;
         }
     }
 
+    int err_cd;
     for (size_t i = 0; i < passed_files; ++i) {
-        cat(opened_files[i]);
+        err_cd = cat(opened_files[i], invisible_to_hex);
         close(opened_files[i]);
     }
     free(opened_files);
-    _exit(0);
+    if (err_cd)
+        _exit(EXIT_FAILURE);
+
+    _exit(EXIT_SUCCESS);
 }
 
-int readbuff(int fd, char *buffer, ssize_t size) {
+ssize_t readbuff(int fd, char *buffer, ssize_t size) {
     ssize_t read_bytes = 0;
-    while (read_bytes < size) {
-        ssize_t just_read = read(fd, buffer + read_bytes, size - read_bytes);
-        if (just_read <= 0) {
+    ssize_t just_read;
+    do {
+        just_read = read(fd, buffer + read_bytes, size - read_bytes);
+        if (just_read < 0) {
             if (errno == EINTR)
                 continue;
             return just_read;
         }
         read_bytes += just_read;
-    }
+    } while (read_bytes < size && just_read != 0);
     return read_bytes;
 }
 
-int writebuff(int fd, const char *buffer, ssize_t size) {
+ssize_t writebuff(int fd, const char *buffer, ssize_t size, bool to_hex) {
+    if (to_hex) {
+        char hexed_buffer[BUFFSIZE * 4];
+        size_t j = 0;
+        for (size_t i = 0; i < size; ++i, ++j)
+            if (!isprint(buffer[i]) && !isspace(buffer[i])) {
+                sprintf(&hexed_buffer[j], "\\x%02x", (unsigned char) buffer[i]);
+                j += 3;
+            } else
+                hexed_buffer[j] = buffer[i];
+        buffer = hexed_buffer;
+        size = j;
+    }
     ssize_t written_bytes = 0;
     while (written_bytes < size) {
         ssize_t just_written = write(fd, buffer + written_bytes, size - written_bytes);
@@ -77,24 +100,20 @@ int writebuff(int fd, const char *buffer, ssize_t size) {
     return written_bytes;
 }
 
-void print(const char *string) {
-    writebuff(1, string, strlen(string));
-}
-
-void error(const char *error_message) {
-    writebuff(2, error_message, strlen(error_message));
+void error(const char *err) {
+    writebuff(STDERR_FILENO, err, strlen(err), false);
 }
 
 void display_help_message() {
-    print(help_message);
+    writebuff(STDOUT_FILENO, help_message, strlen(help_message), false);
     _exit(0);
 }
 
-int cat(int fd) {
+int cat(int fd, bool to_hex) {
     char buffer[BUFFSIZE];
-    int n;
+    ssize_t n;
     while ((n = readbuff(fd, buffer, BUFFSIZE)) > 0)
-        if (writebuff(1, buffer, n) != n) {
+        if (writebuff(STDOUT_FILENO, buffer, n, to_hex) < n) {
             error(write_error);
             return -1;
         }
@@ -102,6 +121,5 @@ int cat(int fd) {
         error(read_error);
         return -1;
     }
-    print("\n");
     return 0;
 }
--- mycat.h
+++ mycat.h
@@ -2,6 +2,7 @@
 #define MYCAT_MYCAT_H
 
 #include <unistd.h>
+#include <stdbool.h>
 
 #define BUFFSIZE 4096
 
@@ -15,14 +16,15 @@ const char help_message[] = "Usage: mycat [OPTION] ... [FILE] ...\n"
 const char open_error[] = "mycat: open error\n";
 const char read_error[] = "mycat: read error\n";
 const char write_error[] = "mycat: write error\n";
+const char malloc_error[] = "mycat: memory allocation error\n";
 
 struct option long_options[] = {{"help", no_argument, NULL, 'h'},
                                 {NULL, 0,             NULL, 0}};
 
 
-int readbuff(int fd, char *buffer, ssize_t size);
+ssize_t readbuff(int fd, char *buffer, ssize_t size);
 
-int writebuff(int fd, const char *buffer, ssize_t size);
+ssize_t writebuff(int fd, const char *buffer, ssize_t size, bool to_hex);
 
 void print(const char *string);
 
@@ -30,6 +32,6 @@ void error(const char *error_message);
 
 void display_help_message();
 
-int cat(int fd);
+int cat(int fd, bool to_hex);
 
 #endif //MYCAT_MYCAT_H
